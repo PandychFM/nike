ccde301a81bfb1c2a40720d4f25083b94633ff6b
Viktor <vitaotkidac@gmail.com>
2024-05-22 13:06:36 +0300

    final

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts.jsx";
+import CustomerReviews from "./CustomerReviews.jsx";
+import Footer from "./Footer.jsx";
+import Services from "./Services.jsx";
+import SpecialOffers from "./SpecialOffers.jsx";
+import Subscribe from "./Subscribe.jsx";
+import SuperQuality from "./SuperQuality.jsx";
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffers,
+    CustomerReviews,
+    Subscribe,
+    Footer,
+}
